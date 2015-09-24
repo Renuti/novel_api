494d85c02bc275f735d06bcc22c3650855200f4b
Ru-T <rwajnberg@gmail.com>
2015-09-24 18:42:57 -0400

    Remove changes from recommendation model.

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -14,9 +14,4 @@ class Recommendation
     HTTParty.get("https://www.tastekid.com/api/#{key}/similar?q=#{@content}&verbose=1")
   end
 
-  def get_recommendation
-    byebug
-    @response["Similar"]["Results"][0]["Name"]
-  end
-
 end
