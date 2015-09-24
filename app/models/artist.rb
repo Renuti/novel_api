74b52b8da51f922a39469c8b6783b83630c2df0e
Ru-T <rwajnberg@gmail.com>
2015-09-24 14:17:33 -0400

    Add files for artist model.

---
+++ app/models/artist.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+require 'byebug'
+
+class Artist < ActiveRecord::Base
+
+  def initialize(show)
+    @artist = artist
+    @response = get_response
+  end
+
+  private def get_response
+    key = ENV['BANDSINTOWN_KEY']
+    HTTParty.get(    "http://api.bandsintown.com/artists/#{@artist}/events.json?api_version=2.0&app_id=#{key}")
+  end
+
+  def get_show_info
+    @response
+
+  end
+
+end
